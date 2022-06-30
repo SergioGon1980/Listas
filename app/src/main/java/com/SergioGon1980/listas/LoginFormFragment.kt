8e6258203de8eb1905e8bb7b898f6ad4935a05d4
SergioGon1980 <sgonzalezoter@gmail.com>
2022-06-30 10:44:35 +0200

    feat: add list activity

--- app/src/main/java/com/SergioGon1980/listas/LoginFormFragment.kt
+++ app/src/main/java/com/SergioGon1980/listas/LoginFormFragment.kt
@@ -1,19 +1,41 @@
 package com.SergioGon1980.listas
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.jocnunez.listas.R
 
 class LoginFormFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("debug", "OnCreate LoginFormFragment")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login_form, container, false)
+        Log.d("debug", "OnCreateView LoginFormFragment: " + context.toString())
+        val viewLogin = inflater.inflate(R.layout.fragment_login_form, container, false)
+        val nextButton = viewLogin.findViewById<Button>(R.id.nextButton)
+
+        nextButton.setOnClickListener {
+            Log.d("debug", "Click on next button")
+            //val intentLista = Intent(context, ListActivity::class.java).apply {
+            //    Log.d("debug", "Intent Lista" + context.toString())
+            //}
+        }
+        val intentLista = Intent(context, ListActivity::class.java).apply {
+            Log.d("debug", "Intent Lista" + context.toString())
+        }
+        startActivity(intentLista)
+        return view
     }
 }
--- app/src/main/java/com/SergioGon1980/listas/MainActivity.kt
+++ app/src/main/java/com/SergioGon1980/listas/MainActivity.kt
@@ -2,11 +2,13 @@ package com.SergioGon1980.listas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.jocnunez.listas.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("debug", "OnCreate MainActivity")
     }
 }
